0a217cce7ad3063d7fc6a0f1be403e2b602a6c18
pfdegorce <pfdegorce@outlook.com>
2023-01-11 13:18:08 +0100

    window qui s'affiche pas

--- include/main.h
+++ include/main.h
@@ -1,8 +1,10 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+//#include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+
+int window();
