6f85d23aa44e3fb99fa4cacf66725b0bbf1660cc
Lorenzo-Tr <endzz2209@gmail.com>
2023-01-11 11:24:23 +0100

    Makefile base

--- include/main.h
+++ include/main.h
@@ -1,5 +1,8 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
