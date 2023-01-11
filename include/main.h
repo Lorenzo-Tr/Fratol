34d4263c4c4f08ec0e81f4750046fe12b4839e4b
cedric <c.darrou@gmx.com>
2023-01-11 14:57:28 +0100

    Remove some includes and the bin

--- include/main.h
+++ include/main.h
@@ -1,9 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
-//#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#include <errno.h>
 #include <stdbool.h>
 
 typedef struct s_parameters{
