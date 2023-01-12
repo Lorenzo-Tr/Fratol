26029f5e2a6c90cfffd6bc29b2eeec7bcc662fce
cedric <c.darrou@gmx.com>
2023-01-12 15:47:22 +0100

    Update Julia.c
    Just work on 0-1

--- src/julia.c
+++ src/julia.c
@@ -2,43 +2,50 @@
 
 #define ITERATION_MAX 150
 
+//Calculate suite
 bool julia_square(t_complex *temp){
 	const t_complex constant = {0.3, 0.5};
 	temp->real = POW(temp->real) - POW(temp->immaginary) + constant.real;
 	temp->immaginary = 2*temp->real*temp->immaginary + constant.immaginary;
-	printf("%f, %f", temp->real, temp->immaginary);
-	if(POW(temp->real) - POW(temp->immaginary) + constant.real > 4){
+	if(POW(temp->real) + POW(temp->immaginary) >= 4){
 		return(false);
 	}
 	return(true);
 }
 
+//Pixel
 static void julia_color_pixel(t_complex *position){
 	int i;
 	bool check;
 	t_complex temp;
 	temp.real = position->real;
 	temp.immaginary = position->immaginary;
-	check = julia_square(&temp);
-	for(i = 0; i < ITERATION_MAX && check; i++){
+	i = 0;
+	check = true;
+	while(i < ITERATION_MAX && check){
 		check = julia_square(&temp);
-		if(check){
+		if(!check){
 			//printf("Pixel noir\n");
+			printf(".");
 		}
+		i++;
 	}
+	//printf("%d\n", i);
 	if(i == ITERATION_MAX){
 		//printf("Pixel blanc\n");
+		printf("x");
 	}
 }
 
-
+//Screen
 void julia(t_env *env){
 	t_complex pixel = {0, 0};
-	for(unsigned int i = 0; i < env->parameters.height; i++){
-		for(unsigned int j = 0; j < env->parameters.weight; j++){
-			pixel.real = i;
-			pixel.immaginary = j;
+	for(unsigned int i = 0; i < env->parameters.weight; i++){
+		for(unsigned int j = 0; j < env->parameters.height; j++){
+			pixel.real = i/env->parameters.weight;
+			pixel.immaginary = j/env->parameters.height;
 			julia_color_pixel(&pixel);
 		}
+		printf("\n");
 	}
 }
